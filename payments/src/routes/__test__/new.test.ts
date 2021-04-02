2aa4e1ecd6ac4686c444f0ac0d640b9d146e5205
kal9isa <docalisa_it@yahoo.com>
2021-04-02 08:02:35 -0700

    returning to old ones1

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -1,73 +1,77 @@
-import mongoose from "mongoose";
 import request from "supertest";
-import { OrderStatus } from "@hiroit/common";
+import mongoose from "mongoose";
 import { app } from "../../app";
 import { Order } from "../../models/order";
-import { stripe } from "../../stripe";
-import { Payment } from "../../models/payment";
+import { OrderStatus } from "@hiroit/common";
+
+jest.mock("../../stripe");
 
 it("returns a 404 when purchasing an order that does not exist", async () => {
   await request(app)
     .post("/api/payments")
     .set("Cookie", global.signin())
     .send({
-      token: "asldkfj",
-      orderId: mongoose.Types.ObjectId().toHexString(),
+      token: "sth",
+      orderId: new mongoose.Types.ObjectId().toHexString(),
     })
     .expect(404);
 });
 
-it("returns a 401 when purchasing an order that doesnt belong to the user", async () => {
+it("returns a 401 when purchasing an order that does not belong to user", async () => {
   const order = Order.build({
-    id: mongoose.Types.ObjectId().toHexString(),
-    userId: mongoose.Types.ObjectId().toHexString(),
+    id: new mongoose.Types.ObjectId().toHexString(),
     version: 0,
+    userId: new mongoose.Types.ObjectId().toHexString(),
     price: 20,
     status: OrderStatus.Created,
   });
+
   await order.save();
 
   await request(app)
     .post("/api/payments")
     .set("Cookie", global.signin())
     .send({
-      token: "asldkfj",
+      token: "sth",
       orderId: order.id,
     })
     .expect(401);
 });
 
-it("returns a 400 when purchasing a cancelled order", async () => {
-  const userId = mongoose.Types.ObjectId().toHexString();
+it("returns a 400 when purchasing an order that is cancelled", async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+
   const order = Order.build({
-    id: mongoose.Types.ObjectId().toHexString(),
-    userId,
+    id: new mongoose.Types.ObjectId().toHexString(),
     version: 0,
+    userId,
     price: 20,
     status: OrderStatus.Cancelled,
   });
+
   await order.save();
 
   await request(app)
     .post("/api/payments")
     .set("Cookie", global.signin(userId))
     .send({
+      token: "sth",
       orderId: order.id,
-      token: "asdlkfj",
     })
     .expect(400);
 });
 
-it("returns a 201 with valid inputs", async () => {
-  const userId = mongoose.Types.ObjectId().toHexString();
-  const price = Math.floor(Math.random() * 100000);
+it("returns a 204 with valid inputs", async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+
   const order = Order.build({
-    id: mongoose.Types.ObjectId().toHexString(),
-    userId,
+    id: new mongoose.Types.ObjectId().toHexString(),
     version: 0,
-    price,
+    userId,
+    price: 20,
     status: OrderStatus.Created,
   });
+
   await order.save();
 
   await request(app)
@@ -78,18 +82,4 @@ it("returns a 201 with valid inputs", async () => {
       orderId: order.id,
     })
     .expect(201);
-
-  const stripeCharges = await stripe.charges.list({ limit: 50 });
-  const stripeCharge = stripeCharges.data.find((charge) => {
-    return charge.amount === price * 100;
-  });
-
-  expect(stripeCharge).toBeDefined();
-  expect(stripeCharge!.currency).toEqual("usd");
-
-  const payment = await Payment.findOne({
-    orderId: order.id,
-    stripeId: stripeCharge!.id,
-  });
-  expect(payment).not.toBeNull();
 });
